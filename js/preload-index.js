0f2dc12835c1792d0b23f8b86d79e223312c85b1
Pelinnnnnn <p.eroglu@gmx.de>
2025-06-25 21:56:39 +0300

    preloadfix2

--- js/preload-index.js
+++ js/preload-index.js
@@ -4,8 +4,7 @@ const coverImages = [
     '../img/cover/cover-hda.png',
     '../img/cover/cover-5050.png',
     '../img/cover/cover-jfs.png',
-    '../img/cover/cover-hdm.png',
-    console.log("preload started")
+    '../img/cover/cover-hdm.png'
 ];
 coverImages.forEach(path => {
     const img = new Image();

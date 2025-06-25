382b9998b9e86ef4f9cb33b7aee6075352358c9e
Pelinnnnnn <p.eroglu@gmx.de>
2025-06-25 21:44:29 +0300

    preoload fix

--- js/preload-index.js
+++ js/preload-index.js
@@ -4,10 +4,12 @@ const coverImages = [
     '../img/cover/cover-hda.png',
     '../img/cover/cover-5050.png',
     '../img/cover/cover-jfs.png',
-    '../img/cover/cover-hdm.png'
+    '../img/cover/cover-hdm.png',
+    console.log("preload started")
 ];
-
 coverImages.forEach(path => {
     const img = new Image();
+    img.onload = () => console.log(`Loaded: ${path}`);
+    img.onerror = () => console.error(`Failed to load: ${path}`);
     img.src = path;
 });
